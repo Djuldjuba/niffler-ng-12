bf0d148d5380db5cabbfad3750a59b715422896c
Aleksey <aleksey.noskov.1998@mail.ru>
2026-07-13 21:42:45 +0300

    add TestMethodContextExtension in homework 10.1

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/TestMethodContextExtension.java
@@ -0,0 +1,27 @@
+package guru.qa.niffler.jupiter.extension;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class TestMethodContextExtension implements BeforeEachCallback, AfterEachCallback {
+
+  private static final ThreadLocal<ExtensionContext> ctxStore = new ThreadLocal<>();
+
+  @Override
+  public void beforeEach(ExtensionContext context) {
+    ctxStore.set(context);
+  }
+
+  @Override
+  public void afterEach(ExtensionContext context) {
+    ctxStore.remove();
+  }
+
+  public static ExtensionContext context() {
+    return ctxStore.get();
+  }
+}
